68686228a3a6a5e62c0d3102a029c2c8f32c79b8
GrabarekM <michael.grabarek@gmail.com>
2026-07-15 17:24:20 +0200

    siemka

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
 <template>
-  <main class="min-h-screen bg-black"></main>
+  <main class="grid min-h-screen place-items-center bg-black">
+    <h1 class="text-5xl font-bold text-white">siemka</h1>
+  </main>
 </template>
